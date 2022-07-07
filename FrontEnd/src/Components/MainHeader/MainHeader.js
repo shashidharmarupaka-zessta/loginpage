c5e0f5bb6b255e323953ca80788160fa389abe01
unknown <VemulaGeetha>
2022-07-07 12:02:40 +0530

    Changes in the main header

--- FrontEnd/src/Components/MainHeader/MainHeader.js
+++ FrontEnd/src/Components/MainHeader/MainHeader.js
@@ -3,9 +3,10 @@ import Navigation from './Navigation';
 import classes from './MainHeader.module.css';
 
 const MainHeader = (props) => {
+  const isLoggedIn = props.isAuthenticated;
   return (
     <header className={classes['main-header']}>
-      <h1>Login Form </h1>  
+      {isLoggedIn ? <h1>User details</h1> : <h1>Login Form </h1>}  
       <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
       
     </header>
